d962804019ceb51aa5193b1ecf4fd71e5e719476
srbtj <srbtj@aliyun.com>
2019-04-30 01:02:47 +0800

    add ci

--- webpack.config.js
+++ webpack.config.js
@@ -1,3 +1,12 @@
-module.exports = {
-  
+const webpackMerge = require('webpack-merge');
+const parse = require('yargs-parser');
+const _mode = parse(process.argv).mode;
+const _merge = require(require(__dirname + `/config/webpack.${_mode}.js`));
+
+// 入口文件
+const _entry = {};
+const webpackConfig = {
+  entry: _entry
 };
+
+module.exports = webpackMerge(webpackConfig, _merge);
